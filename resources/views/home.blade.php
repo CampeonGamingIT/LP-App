208d04fd0e5f03399a67357a3ac12f0ca620daa8
AlexCampeon <alexandercampeongaming@gmail.com>
2024-09-20 16:14:10 +0300

    css changes from tw to bs classes in home component

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,45 +9,63 @@
     <title>Home</title>
 </head>
 <body>
-    <div class="flex justify-center mt-3">
-        <h1 class="text-3xl font-bold underline">Landing Pages App</h1>
-
+    <div class="d-flex justify-content-center mt-3">
+        <h1 class="display-6 fw-bold text-decoration-underline">Landing Pages App</h1>
     </div>
 
-    <div class="grid grid-rows-8">
-        <form action="" method="POST" id="form">
+    <div class="container-fluid">
+        <form action="" method="POST" id="form" class="">
             @csrf
-            <div>
-                <label for="url" class="ms-2">Write here the url: </label>
-                <input type="text" name="url" id="url" class="border border-gray-400 ms-1.5 mt-0.5" required>
+            <div class="row">
+                <div class="col-12 mt-1">
+                    <label for="url">Write here the url: </label>
+                    <input type="text" name="url" id="url" class="border" required>
+                </div>
             </div>
 
-            <div>
-                <label for="favicon" class="ms-2">Write here the favicon: </label>
-                <input type="text" name="favicon" id="favicon" class="border border-gray-400 ms-1.5 mt-0.5" required>
+
+            <div class="row">
+                <div class="col-12 mt-1">
+                    <label for="favicon">Write here the favicon: </label>
+                    <input type="text" name="favicon" id="favicon" class="border" required>
+                </div>
             </div>
 
-            <div>
-                <label for="pageLogo" class="ms-2">Write here the page logo: </label>
-                <input type="text" name="pageLogo" id="pageLogo" class="border border-gray-400 ms-1.5 mt-0.5" required>
+            <div class="row">
+                <div class="col-12 mt-1">
+                    <label for="pageLogo">Write here the page logo: </label>
+                    <input type="text" name="pageLogo" id="pageLogo" class="border" required>
+                </div>
+
             </div>
 
-            <div>
-                <label for="pageTitle" class="ms-2">Write here the page title: </label>
-                <input type="text" name="pageTitle" id="pageTitle" class="border border-gray-400 ms-1.5 mt-0.5" required>
+            <div class="row">
+                <div class="col-12 mt-1">
+                    <label for="pageTitle">Write here the page title: </label>
+                    <input type="text" name="pageTitle" id="pageTitle" class="border" required>
+                </div>
             </div>
 
-            <div>
-                <label for="ctaLink" class="ms-2">Write here the cta link: </label>
-                <input type="text" name="ctaLink" id="ctaLink" class="border border-gray-400 ms-1.5 mt-0.5" required>
+            <div class="row">
+                <div class="col-12 mt-1">
+                    <label for="ctaLink">Write here the cta link: </label>
+                    <input type="text" name="ctaLink" id="ctaLink" class="border" required>
+                </div>
             </div>
 
-            <div>
-                <label for="bgImg" class="ms-2">Write here the bg IMG: </label>
-                <input type="text" name="bgImg" id="bgImg" class="border border-gray-400 ms-1.5 mt-0.5" required>
+            <div class="row">
+                <div class="col-12 mt-1">
+                    <label for="bgImg">Write here the bg IMG: </label>
+                    <input type="text" name="bgImg" id="bgImg" class="border" required>
+                </div>
             </div>
 
-            <x-primary-button class="mt-3 bg-danger">Submit</x-primary-button>
+            <div class="row mt-3 ms-1">
+                <div class="col-2 d-flex justify-content-center">
+                    <button class="bg-danger text-white border-danger">Submit</button>
+                </div>
+            </div>
+
         </form>
     </div>
 
