e9d4a53e7497fe9c36d4c702dbaeeeac729ca074
AlexCampeon <alexandercampeongaming@gmail.com>
2024-09-20 13:56:07 +0300

    added head/header/main/features/footer components

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -2,7 +2,7 @@
 <header>
     <div class="container py-2">
         <div class=" text-center text-md-start">
-            <img class="logo img-fluid" src="<?php echo $page_logo; ?>"/>
+            <img class="logo img-fluid" src="{{$page_logo}}" alt="page logo"/>
         </div>
     </div>
 </header>
