0ebb973deb33e081968eaa58bd76137d2e526e4f
AlexCampeon <alexandercampeongaming@gmail.com>
2024-09-25 15:17:50 +0300

    add JS function in resources file

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,3 +6,20 @@ import 'bootstrap';
 
 // Added: Popper.js dependency for popover support in Bootstrap
 import '@popperjs/core';
+
+
+/**
+ *  A function that copies the text via clicking
+ * @param text  The parameter that will be copied
+ * @returns {boolean}   If there was something to be copier or not
+ */
+
+function copy(text) {
+    var input = document.createElement('textarea'); // Create a text area element
+    input.innerHTML = text; // Copy the given parameter to the text area
+    document.body.appendChild(input);   // Append it to the DOM
+    input.select(); // Select the text area element
+    var result = document.execCommand('copy');  // Copy to the clipboard the text of text area
+    document.body.removeChild(input);   // Remove the text area from DOM
+    return result;  // Return if it was successful or not
+}
