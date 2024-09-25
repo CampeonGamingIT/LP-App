f9ce281c0d042d77ce0f91d770ae324430ff02b6
AlexCampeon <alexandercampeongaming@gmail.com>
2024-09-25 15:16:21 +0300

    add index - create - post(create)

--- app/Http/Controllers/LandingPageController.php
+++ app/Http/Controllers/LandingPageController.php
@@ -6,28 +6,26 @@ use Illuminate\Http\Request;
 
 class LandingPageController extends Controller
 {
-    /**
-     * Handle the incoming request.
-     */
-    public function __invoke(Request $request, $domain, $path, $lang, $curr, $query_string)
+
+    public function index()
     {
-        request()->validate([
-            'favicon' => ['required'],
-            'pageLogo' => ['required'],
-            'pageTitle' => ['required'],
-            'ctaLink' => ['required'],
-            'bgIMG' => ['required']
+        return view('home');
+    }
+
+    public function create()
+    {
+        return view('create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            $lp_domain = 'lp_domain' => ['required', 'url' => 'url:https'],
+            $favicon = 'favicon' => ['required', 'url' => 'url:https'],
+            $page_logo = 'page_logo' => ['required', 'url' => 'url:https'],
+            $page_title = 'page_title' => ['required', 'string']
         ]);
 
-        // Extracts the url's parameters
-        $domain = $request->route('domain');
-        $path = $request->route('path');
-        $lang = $request->route('lang');
-        $curr = $request->route('curr');
-        $query_string = $request->route('query_string');
-
-
-        // TODO create a view single or along with components (FE part)
-        return view('', compact('domain', 'path', 'lang', 'curr', 'query_string'));
+        return view('landing-page', ['lp_domain' => $lp_domain, 'favicon' => $favicon, 'page_logo' => $page_logo, 'page_title' => $page_title]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
 use App\Http\Controllers\LandingPageController;
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'home');
-Route::post('/{domain}/{path}/{lang}/{curr}/{query_string}', LandingPageController::class);
+Route::get('/', [LandingPageController::class, 'index']);
+Route::get('/landings/create', [LandingPageController::class, 'create']);
+Route::post('/landings', [LandingPageController::class, 'store']);
+//Route::post('/{domain}/{path}/{lang}/{curr}/{query_string}', LandingPageController::class);
