f801db3c1ac6fce643dcf41089f49a0e24dd14cb
AlexCampeon <alexandercampeongaming@gmail.com>
2024-09-27 13:28:19 +0300

    Revert the routes to include laravel's user system

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,16 @@ Route::get('/', [LandingPageController::class, 'index']);
 Route::get('/landings/create', [LandingPageController::class, 'create']);
 Route::post('/landings', [LandingPageController::class, 'store']);
 //Route::post('/{domain}/{path}/{lang}/{curr}/{query_string}', LandingPageController::class);
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
